631e59af944b02770df82ac37d6f5e8482f8d3dc
Pavel Leonenko <pavel.leonenko@softeq.com>
2021-09-18 13:55:54 +0300

    Experimenting with args

--- args_test.c
+++ args_test.c
@@ -1,49 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-void memoryLeakTest();
 
 int main(argc, argv)
     int argc; 
     char **argv;
 {
-    // memoryLeakTest();
-
     printf("argc: %d\n", argc);
 
     for (int i = 0; i < argc; i++)
     {
         int j = 0;
-        char symbol = **(argv + i);
+        /* Why argv+i instead argv+i*sizeof(char*)?
+         * Because when ++ is called for an instance of char**, it's automatically 
+         * increased by sizeof(char*).
+         * 
+         printf("%ld %d %ld\n", argv, i, (char**)(void*)((long)(void*)argv + i));
+         char *addr = *((char**)(void*)((long)(void*)argv + i)); */
+        printf("%ld %d %ld\n", (long)argv, i, (long)(argv + i));
+        char *addr = *(argv + i);
+        printf("0x%lX\n", (long)addr);
+        char symbol = *addr;
         do
         {
             printf("%c", symbol);
             j++;
-            symbol = *(*(argv + i) + j);
+            char co = 'n';
+            symbol = co;
+            symbol = *(addr + j);
         }
         while (symbol != 0);
 
         printf("\n");
     }
 
-    for (int i = 0; i < argc; i++)
-    {
-        printf("%s\n", *(argv + i));
-    }
-
     printf("End of program.\n");
-}
-
-void memoryLeakTest()
-{
-    int index = 0;
-    while (1)
-    {
-        malloc(1 << 30);
-        index++;
-        if (index % 1000 == 0)
-        {
-            printf("%i\n", index);
-        }
-    }
 }
